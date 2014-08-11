ee475e968d32e8c0fc0c5bd547afcdab157ccf7e
Ben Cherry <bcherry@gmail.com>
2014-08-11 15:19:37 -0700

    Strip nil values before to_json

--- spec/apns_dispatch/apn_notification_spec.rb
+++ spec/apns_dispatch/apn_notification_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe ApnsDispatch::ApnNotification do
-  before do
+  it 'sends a notification packet to the connection' do
     token = '7307dea69c58e9625250aabb040594e1a031f574dba5d97549fbe211327aff4c'
     message = 'test message'
     options = { aps: { badge: 1 }, order_id: 1 }
@@ -11,9 +11,23 @@ describe ApnsDispatch::ApnNotification do
     expected_message = { aps: { alert: message, sound: 'default', badge: 1}, order_id: 1}
     payload = expected_message.to_json
     @packet = [0, 32, [token].pack('H*'), payload.bytesize, payload].pack('cna*na*')
+
+    @connection.should_receive(:write).with(@packet)
+
+    @apn_notificiation.send_notification
   end
 
-  it 'sends a notification packet to the connection' do
+  it 'strips nil values' do
+    token = '7307dea69c58e9625250aabb040594e1a031f574dba5d97549fbe211327aff4c'
+    message = 'test message'
+    options = { aps: { badge: 1, :sound => nil }, order_id: 1 }
+    @connection = stub
+    @apn_notificiation = ApnsDispatch::ApnNotification.new(@connection, token, message, options)
+
+    expected_message = { aps: { alert: message, badge: 1}, order_id: 1}
+    payload = expected_message.to_json
+    @packet = [0, 32, [token].pack('H*'), payload.bytesize, payload].pack('cna*na*')
+
     @connection.should_receive(:write).with(@packet)
 
     @apn_notificiation.send_notification
